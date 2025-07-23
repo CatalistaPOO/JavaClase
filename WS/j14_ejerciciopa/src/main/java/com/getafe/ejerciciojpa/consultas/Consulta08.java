e34c61f7b9b925758271a328153cb2f6c9fa65b1
CatalistaPOO <fgtpoo78@gmail.com>
2025-07-23 14:00:39 +0200

    23jul

--- WS/j14_ejerciciopa/src/main/java/com/getafe/ejerciciojpa/consultas/Consulta08.java
+++ WS/j14_ejerciciopa/src/main/java/com/getafe/ejerciciojpa/consultas/Consulta08.java
@@ -3,23 +3,27 @@ package com.getafe.ejerciciojpa.consultas;
 
 import java.util.List;
 import com.getafe.ejerciciojpa.config.Config;
+
 import com.getafe.ejerciciojpa.modelo.ClienteDTO;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.TypedQuery;
 
-public class Consulta07 {
+public class Consulta08 {
 	public static void main(String[] args) {
 		
-		clienteDTO().forEach(System.out::println);
+		prodClientes("cable").forEach(System.out::println);
 		
 		}
 		
-	public static List<ClienteDTO> clienteDTO() {
+	
+	public static List<ClienteDTO> prodClientes(String producto) {
 		EntityManager em = Config.getEmf().createEntityManager();
 		//Devolver todos los clientes usando clienteDTO
-		String jpql = "select com.getafe.ejerciciojpa.modelo(c.idRol, c.idCliente, c.persona.apellidos, c.nroCliente, c.categoria)" 
-				+ " from Cliente c";//consulta JPQL llamando al constructor (del record (el path de la claseClienteCategoria))
+		String jpql = "select c.idRol, c.persona.apellidos, c.nroCliente, c.categoria" 
+				+ "from Cliente c join c.productos p where p.producto like :prod";//consulta JPQL llamando al constructor (del record (el path de la claseClienteCategoria))
+		
 		TypedQuery<ClienteDTO> q = em.createQuery(jpql, ClienteDTO.class);
+		q.setParameter("prod" , "%" + producto + "%");
 		
 		return q.getResultList();
 	}
