6c4f9d21da0d760443846e21eaf42f2e05604076
F <poofgt@gmail.com>
2025-09-08 12:45:23 +0200

    8septiembre

---
+++ WS/w03_tienda/src/main/java/com/getafe/tienda/excepciones/PersistenciaException.java
@@ -0,0 +1,31 @@
+package com.getafe.tienda.excepciones;
+
+public class PersistenciaException extends RuntimeException {
+
+	public PersistenciaException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public PersistenciaException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public PersistenciaException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public PersistenciaException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public PersistenciaException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
