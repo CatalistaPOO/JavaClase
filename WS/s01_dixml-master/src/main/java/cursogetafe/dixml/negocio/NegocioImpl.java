8553b83d7bdf27a55edf23c668771957e09270fa
CatalistaPOO <fgtpoo78@gmail.com>
2025-09-16 16:51:08 +0200

    spring project added

---
+++ WS/s01_dixml-master/src/main/java/cursogetafe/dixml/negocio/NegocioImpl.java
@@ -0,0 +1,40 @@
+package cursogetafe.dixml.negocio;
+
+import cursogetafe.dixml.persistencia.ClienteDao;
+
+public class NegocioImpl implements Negocio{
+
+	
+//	final private ClienteDao cDao (puede initializar pero hay que dar un valor en un constructor luego borrar el setter);
+	private ClienteDao cDao;
+	
+	public NegocioImpl(){
+//		cDao = null;
+		System.out.println("Constructor por defecto del NegocioImp");
+	}
+	
+	
+	public NegocioImpl(ClienteDao cDao) {
+		this.cDao = cDao;
+		System.out.println("Constructor  del NegocioImp(ClienteDao cDao)");
+	}
+	
+
+	public ClienteDao getcDao() {
+		return cDao;
+	}
+
+
+	public void setcDao(ClienteDao cDao) {
+		System.out.println("NegocioImpl, setcDao(...)");
+		this.cDao = cDao;
+	}
+
+
+	@Override
+	public String proceso(int idCliente) {
+		return "Soy el negocio y llamo al dao: " + cDao.findById(idCliente);
+	}
+	
+
+}
